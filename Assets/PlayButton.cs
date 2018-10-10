78cb24fb3265763063bcddb10355c394375d4f61
DESKTOP-1PIH112\Alan <alancasanova@hotmail.it>
2018-10-10 15:27:26 +0200

    no message

---
+++ Assets/PlayButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour {
+
+    public string SceneToSwitchTo;
+
+    public void SwitchToScene()
+    {
+        SceneManager.LoadScene(SceneToSwitchTo);
+    }
+}
