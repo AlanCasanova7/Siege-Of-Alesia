0a3064a1a162d9442087185db9f523310da25f49
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2018-10-10 12:04:41 +0200

    Created timer + FloatValues.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class Timer : MonoBehaviour
+{
+    public UnityEvent OnTimerFinished;
+
+    public FloatValue Duration;
+
+    public FloatValue CurrentValue;
+
+    public bool SpawnAsEnabled = false;
+
+    private float timer;
+
+    private void Awake()
+    {
+        enabled = SpawnAsEnabled;
+    }
+    void OnEnable()
+    {
+        timer = 0f;
+
+        if (!CurrentValue || !Duration)
+            throw new NullReferenceException("Timer requires non null FloatValues references.");
+
+        CurrentValue.Value = timer;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        CurrentValue.Value = timer;
+
+        if (timer > Duration.Value)
+        {
+            this.enabled = false;
+            OnTimerFinished.Invoke();
+        }
+    }
+}
