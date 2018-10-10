ab673551b378a0a81f4c3e8b1ea18de9bf93573f
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2018-10-10 15:37:10 +0200

    Added ragdoll

---
+++ Assets/Scripts/PhysicsHolder.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+[System.Serializable]
+public class PhysicsHolder
+{
+    public Rigidbody Body;
+    public Collider Collider;
+    public CharacterJoint Joint;
+}
---
+++ Assets/Scripts/RagdollHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollHandler : MonoBehaviour
+{
+
+    public PhysicsHolder[] Bodies = new PhysicsHolder[0];
+    public bool StartWithPhysicsEnabled = false;
+
+    void OnEnable()
+    {
+        ChangePhysics(StartWithPhysicsEnabled);
+    }
+
+    public void ChangePhysics(bool value)
+    {
+        for (int i = 0; i < Bodies.Length; i++)
+        {
+            PhysicsHolder holder = Bodies[i];
+            if (holder.Collider)
+            {
+                holder.Collider.enabled = value;
+            }
+            if (holder.Joint)
+            {
+                holder.Joint.enableCollision = value;
+                holder.Joint.enablePreprocessing = value;
+                holder.Joint.enableProjection = value;
+            }
+            if (holder.Body)
+            {
+                holder.Body.isKinematic = !value;
+                holder.Body.useGravity = value;
+            }
+        }
+    }
+    public void ActivatePhysics()
+    {
+        ChangePhysics(true);
+    }
+    public void DeactivatePhysics()
+    {
+        ChangePhysics(false);
+    }
+}
