be0f64025fc3b6b6815429de0ee1d098e496e25a
fulmyne <fulminet@live.it>
2018-10-10 18:01:31 +0200

    cards UI whit logic , remember to open the CardLogic script for delete the lines  13  and  30 to 40

---
+++ Assets/CardLogic.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardLogic : MonoBehaviour
+{
+    public Image BG_Image;
+    public Image Cover_Image;
+    public Image Number_Image;
+
+    public Sprite Sprite;
+
+    public void ShowChoosedCard(Sprite sprite)
+    {
+        BG_Image.sprite = sprite;
+        Cover_Image.enabled = false;
+        Number_Image.enabled = false;
+    }
+
+    public void ResetCard()
+    {
+        BG_Image.sprite = null;
+        Cover_Image.enabled = true;
+        Number_Image.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ShowChoosedCard(Sprite);
+        }
+        else
+        {
+            ResetCard();
+        }
+    }
+}
