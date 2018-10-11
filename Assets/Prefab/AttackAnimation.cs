36c83bff9276717ad50e8ce463267c6c0d3ebfba
DESKTOP-1PIH112\Alan <alancasanova@hotmail.it>
2018-10-11 12:12:37 +0200

    drbdgd

---
+++ Assets/Prefab/AttackAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAnimation : MonoBehaviour
+{
+    public Transform RegroupPoint;
+    private Vector3 regroupPosition;
+
+    private PopulationManager populationManager;
+
+	void Start ()
+    {
+        regroupPosition = RegroupPoint.position;
+        populationManager = this.GetComponentInChildren<PopulationManager>();
+	}
+
+    public void FireAnimation()
+    {
+        populationManager.StartAnimation(regroupPosition);
+    }
+}
