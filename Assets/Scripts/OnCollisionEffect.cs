45f53d1ca21a1313b039a6eaf6819d9ac998cbba
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2018-10-12 13:42:43 +0200

    ff:x

--- Assets/Scripts/OnCollisionEffect.cs
+++ Assets/Scripts/OnCollisionEffect.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class OnCollisionEffect : MonoBehaviour
 {
-
+    public PopulationManager Manager;
     public ParticleSystem Effect;
+    public int Percentage = 20;
     private void OnCollisionEnter(Collision collision)
     {
-        if (Effect)
+        if (Random.Range(0, 101) < Percentage)
             GameObject.Instantiate(Effect, this.transform);
     }
 }
