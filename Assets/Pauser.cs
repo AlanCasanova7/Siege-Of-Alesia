b34007e5e8afa384a53582fe382b90eb6945ecb7
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2018-10-12 09:23:31 +0200

    Fixed pause menu bug where the game would not unpause if returning to main menu

--- Assets/Pauser.cs
+++ Assets/Pauser.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class Pauser : MonoBehaviour
 {
-
+    public bool Paused { get; private set; }
     public GameObject PauseMenu;
     public KeyCode PauseButton;
 
     float prevTimeScale;
     private void Start()
     {
+        Paused = false;
         PauseMenu.SetActive(false);
     }
     private void Update()
     {
         if (Input.GetKeyDown(PauseButton))
         {
-            if (PauseMenu.activeSelf)
+            if (Paused)
                 UnpauseGame();
             else
                 PauseGame();
@@ -26,15 +27,18 @@ public class Pauser : MonoBehaviour
 
     public void PauseGame()
     {
-        if (PauseMenu.activeSelf)
+        if (Paused)
             return;
-
+        Paused = true;
         prevTimeScale = Time.timeScale;
         Time.timeScale = 0f;
         PauseMenu.SetActive(true);
     }
     public void UnpauseGame()
     {
+        if (!Paused)
+            return;
+        Paused = false;
         Time.timeScale = prevTimeScale;
         PauseMenu.SetActive(false);
     }
