34689ddb8d16b75fb993bbd888662d2c991bd7e9
Matteo Lorenzo Nasci <Matteo.Lorenzo.Nasci@hotmail.com>
2018-10-12 16:43:04 +0200

    end screen

--- Assets/Scripts/GameOverTextShower.cs
+++ Assets/Scripts/GameOverTextShower.cs
@@ -8,11 +8,22 @@ public class GameOverTextShower : MonoBehaviour
     public GameResult GameOverResults;
     public Text PlayerShower;
     public Text StatsShower;
+    public AudioSource Source;
+    public AudioClip[] Clips;
+
+    public string[] Citations;
+    public string GalliPhrase;
+    public string RomansPhrase;
+    public Text CitShower;
 
     void Start()
     {
-        PlayerShower.text = "The winner is PLAYER" + (GameOverResults.IndexWinner + 1) + " with " + GameOverResults.FervorWinner + " fervor and " + GameOverResults.PopulationWinner + " population left!!! Congratz!";
+        Source.clip = Clips[GameOverResults.IndexWinner];
+        Source.time = 0f;
+        Source.Play();
+        PlayerShower.text = (GameOverResults.IndexWinner == 0 ? RomansPhrase : GalliPhrase);
         StatsShower.text = "The game lasted for " + GameOverResults.TotalGameTurns + " turns, for a total duration of " + GameOverResults.TotalTime + " seconds.";
+        CitShower.text = Citations[Random.Range(0, Citations.Length)];
     }
 
 }
